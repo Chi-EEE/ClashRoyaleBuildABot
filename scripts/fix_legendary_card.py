2f1b436df36831edb29b7f04f8d6d88f6238ca70
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-01-11 22:49:59 +0000

    Removed unneeded code
    Since "fix_legendary_card.py" is only going to be used on legendary cards, it is unnecessary to check if the area has black area to convert to transparent pixels.

--- scripts/fix_legendary_card.py
+++ scripts/fix_legendary_card.py
@@ -7,7 +7,6 @@ import os
 WIDTH = 195
 HEIGHT = 250
 # 
-BGR_THRESHOLD = 15
 
 script_location = os.path.dirname(os.path.realpath(__file__))
 
@@ -18,15 +17,8 @@ inputs = sys.argv[1:]
 # read mask image
 mask = cv2.imread(rf'{script_location}\legendary_card_mask.png', 0)
 
-# Get the black pixels of the mask image
 _, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)
 mask = cv2.resize(mask, (WIDTH,HEIGHT))
-        
-# Add an extra dimension in the last axis.
-mask = np.expand_dims(mask, axis=-1)
-
-# Inverts the mask
-inverted_mask = cv2.bitwise_not(mask)
 
 for arg in inputs:
     if arg.endswith(".png"):
@@ -36,22 +28,14 @@ for arg in inputs:
         # Resize the image
         original_image = cv2.resize(original_image, (WIDTH,HEIGHT))
         
-        # Gets the inverted masked area of the original image
-        transparent_crop = cv2.bitwise_and(original_image, original_image, mask=inverted_mask)
+        # Copy original image
+        result = original_image.copy()
         
-        original_area = np.sum(mask, axis=-1)
+        # Convert the image to BGRA 
+        result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
         
-        # Make a True/False mask of pixels whose BGR values sum to more than BGR_THRESHOLD
-        alpha = np.sum(transparent_crop, axis=-1) > BGR_THRESHOLD
-
-        # If the pixel is not transparent then keep it
-        original_and_alpha_area = (np.logical_or(original_area, alpha))
-        
-        # Convert True/False to 0/255 and change type to "uint8" to match "original_image"
-        original_and_alpha_area = np.uint8(original_and_alpha_area * 255)
-
-        # Add the alpha channel to the original image
-        result = np.dstack((original_image, original_and_alpha_area))
+        # Insert the alpha channel
+        result[:, :, 3] = mask
 
         # Save result
         path = rf"{script_location}\out\{os.path.basename(arg)}"
