96ca5f0a9a39125b74f2566e56f759c2825cb3cd
Huu Chi Huynh <73843190+Chi-EEE@users.noreply.github.com>
2023-01-11 17:54:56 +0000

    Fixed script unable to output mask changes
    Fixed script creating out folder in different directory if not executed in same directory.

--- scripts/resize.py
+++ scripts/resize.py
@@ -2,17 +2,21 @@ import sys
 import cv2
 import numpy as np
 import os
+script_location = os.path.dirname(os.path.realpath(__file__))
 
-if not os.path.exists(r"scripts\out"):
-    os.mkdir(r"scripts\out")
+if not os.path.exists(rf"{script_location}\out"):
+    os.mkdir(rf"{script_location}\out")
 inputs = sys.argv[1:]
 
 # read mask image
-mask = cv2.imread(r'scripts\legendary_card_mask.png', 0)
+mask = cv2.imread(rf'{script_location}\legendary_card_mask.png', 0)
 
 # Get the black pixels of the mask image
 _, mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY)
 mask = cv2.resize(mask, (195,250))
+
+# Inverts the mask
+inverted_mask = cv2.bitwise_not(mask)
 
 for arg in inputs:
     if arg.endswith(".png"):
@@ -22,23 +26,27 @@ for arg in inputs:
         # Resize the image
         original_image = cv2.resize(original_image, (195,250))
         
-        inverted_mask = cv2.bitwise_not(mask)
-        
-        # original_crop = cv2.bitwise_and(original_image, original_image, mask=mask)
+        # Gets the inverted masked area of the original image
         transparent_crop = cv2.bitwise_and(original_image, original_image, mask=inverted_mask)
         
-        # Make a True/False mask of pixels whose BGR values sum to more than zero
-        alpha = np.sum(transparent_crop, axis=-1) > 5
+        # Add an extra dimension in the last axis.
+        mask = np.expand_dims(mask, axis=-1)
+        
+        original_area = np.sum(mask, axis=-1)
+        
+        # Make a True/False mask of pixels whose BGR values sum to more than five
+        alpha = np.sum(transparent_crop, axis=-1) > 15
 
+        # If the pixel is not transparent then keep it
+        original_and_alpha_area = (np.logical_or(original_area, alpha))
+        
         # Convert True/False to 0/255 and change type to "uint8" to match "original_image"
-        alpha = np.uint8(alpha * 255)
+        original_and_alpha_area = np.uint8(original_and_alpha_area * 255)
 
-        alpha = cv2.merge([alpha]*3)
-
-        result = original_image.copy()
-        result[inverted_mask!=0] = alpha[inverted_mask!=0]
+        # Add the alpha channel to the original image
+        result = np.dstack((original_image, original_and_alpha_area))
 
         # Save result
-        path = rf"scripts\out\{os.path.basename(arg)}"
-        print("saving image at :", path)
+        path = rf"{script_location}\out\{os.path.basename(arg)}"
+        # print("saving image at :", path)
         cv2.imwrite(path, result)
